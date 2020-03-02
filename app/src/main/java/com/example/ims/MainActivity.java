b3f4252e5850727819a581c2a96f301f82ae3def
abdelaziz <abdelaz9z>
2020-03-03 00:49:54 +0200

    Add the navigation

--- app/src/main/java/com/example/ims/MainActivity.java
+++ app/src/main/java/com/example/ims/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.ims;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.zip.Inflater;
 
-    CardView receptionCardView,
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private CardView receptionCardView,
             theDoctorCardView,
             analysisLabCardView,
             radiologyLaboratoryCardView,
             thePharmacyCardView,
             financialAccountsCardView,
             personnelCardView;
+
+    private ImageButton actionMenuImageButton;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +42,26 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionMenuImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
         // Reception
         receptionCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Reception", Toast.LENGTH_SHORT).show();
+                // To reception
+                Intent intent = new Intent(MainActivity.this, ReceptionActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -35,7 +69,9 @@ public class MainActivity extends AppCompatActivity {
         theDoctorCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "The doctor", Toast.LENGTH_SHORT).show();
+                // To the doctor
+                Intent intent = new Intent(MainActivity.this, TheDoctorActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -43,7 +79,9 @@ public class MainActivity extends AppCompatActivity {
         analysisLabCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Analysis lab", Toast.LENGTH_SHORT).show();
+                // To analysis lab
+                Intent intent = new Intent(MainActivity.this, AnalysisLabActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -51,7 +89,9 @@ public class MainActivity extends AppCompatActivity {
         radiologyLaboratoryCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Radiology laboratory", Toast.LENGTH_SHORT).show();
+                // To radiology laboratory
+                Intent intent = new Intent(MainActivity.this, RadiologyLaboratoryActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -59,7 +99,9 @@ public class MainActivity extends AppCompatActivity {
         thePharmacyCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "The Pharmacy", Toast.LENGTH_SHORT).show();
+                // To the Pharmacy
+                Intent intent = new Intent(MainActivity.this, ThePharmacyActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -67,7 +109,9 @@ public class MainActivity extends AppCompatActivity {
         financialAccountsCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Financial accounts", Toast.LENGTH_SHORT).show();
+                // To financial accounts
+                Intent intent = new Intent(MainActivity.this, FinancialAccountsActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -75,14 +119,72 @@ public class MainActivity extends AppCompatActivity {
         personnelCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Personnel", Toast.LENGTH_SHORT).show();
+                // To personnel
+                Intent intent = new Intent(MainActivity.this, PersonnelActivity.class);
+                startActivity(intent);
             }
         });
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Intent intent;
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.menu_item_home:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_reception:
+                intent = new Intent(this, ReceptionActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_the_doctor:
+                intent = new Intent(this, TheDoctorActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_analysis_lab:
+                intent = new Intent(this, AnalysisLabActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_radiology_laboratory:
+                intent = new Intent(this, RadiologyLaboratoryActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_the_pharmacy:
+                intent = new Intent(this, ThePharmacyActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_financial_accounts:
+                intent = new Intent(this, FinancialAccountsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_personnel:
+                intent = new Intent(this, PersonnelActivity.class);
+                startActivity(intent);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     // initial
     private void init() {
+
+        drawerLayout = findViewById(R.id.activity_main);
+        navigationView = findViewById(R.id.navigation_view);
+
+        actionMenuImageButton = findViewById(R.id.image_button_action_menu);
+
         receptionCardView = findViewById(R.id.card_view_reception);
         theDoctorCardView = findViewById(R.id.card_view_the_doctor);
         analysisLabCardView = findViewById(R.id.card_view_analysis_lab);
@@ -92,3 +194,5 @@ public class MainActivity extends AppCompatActivity {
         personnelCardView = findViewById(R.id.card_view_personnel);
     }
 }
+
+
---
+++ app/src/main/java/com/example/ims/TheDoctorActivity.java
@@ -0,0 +1,103 @@
+package com.example.ims;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class TheDoctorActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    private ImageButton actionMenuImageButton;
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_doctor);
+
+        init();
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionMenuImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Intent intent;
+        int id = menuItem.getItemId();
+        switch (id) {
+            case R.id.menu_item_home:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_reception:
+                intent = new Intent(this, ReceptionActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_the_doctor:
+                intent = new Intent(this, TheDoctorActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_analysis_lab:
+                intent = new Intent(this, AnalysisLabActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_radiology_laboratory:
+                intent = new Intent(this, RadiologyLaboratoryActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_the_pharmacy:
+                intent = new Intent(this, ThePharmacyActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_financial_accounts:
+                intent = new Intent(this, FinancialAccountsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_personnel:
+                intent = new Intent(this, PersonnelActivity.class);
+                startActivity(intent);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // initial
+    private void init() {
+        drawerLayout = findViewById(R.id.activity_the_doctor);
+        navigationView = findViewById(R.id.navigation_view);
+
+        actionMenuImageButton = findViewById(R.id.image_button_action_menu);
+
+    }
+
+}
