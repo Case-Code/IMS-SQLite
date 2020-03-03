d6c24746efdf0933d8a690e79f817cae6093de3a
abdelaziz <abdelaz9z>
2020-03-03 22:58:16 +0200

    Transferred to the analysis lab

--- app/src/main/java/com/example/ims/ReceptionActivity.java
+++ app/src/main/java/com/example/ims/ReceptionActivity.java
@@ -1,10 +1,11 @@
 package com.example.ims;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -13,13 +14,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDialog;
-import android.support.v7.app.AppCompatDialogFragment;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class ReceptionActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -28,6 +30,7 @@ public class ReceptionActivity extends AppCompatActivity implements NavigationVi
     private ImageButton mActionMenuImageButton;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private FloatingActionButton mFloatingActionButton;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +57,7 @@ public class ReceptionActivity extends AppCompatActivity implements NavigationVi
                 showPatientRegistrationDialog();
             }
         });
+
     }
 
     @Override
@@ -114,6 +118,7 @@ public class ReceptionActivity extends AppCompatActivity implements NavigationVi
         mFloatingActionButton = findViewById(R.id.floating_action_button);
     }
 
+    // Show patient registration dialog
     private void showPatientRegistrationDialog() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         View mView = getLayoutInflater().inflate(R.layout.dialog_patient_registration, null);
@@ -125,6 +130,22 @@ public class ReceptionActivity extends AppCompatActivity implements NavigationVi
         final EditText locationEditText = mView.findViewById(R.id.edit_location);
         final EditText weightEditText = mView.findViewById(R.id.edit_weight);
         final EditText heightEditText = mView.findViewById(R.id.edit_height);
+        final ImageButton actionDateImageButton = mView.findViewById(R.id.image_button_action_date);
+
+        actionDateImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month += 1;
+                        String date = month + "/" + dayOfMonth + "/" + year;
+                        birthDateEditText.setText(date);
+                    }
+                };
+                showDatePicker(dateSetListener);
+            }
+        });
 
         builder.setView(mView);
         builder.setTitle("Patient registration");
@@ -150,6 +171,65 @@ public class ReceptionActivity extends AppCompatActivity implements NavigationVi
 
     // Patient registration
     private void patientRegistration() {
+
+    }
+
+    /**
+     * View and set the date-picker
+     * 1. DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+     * 2.  month += 1;
+     * 3. String date = month + "/" + dayOfMonth + "/" + year;
+     *
+     * @param mDateSetListener
+     */
+    private void showDatePicker(DatePickerDialog.OnDateSetListener mDateSetListener) {
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                android.R.style.Theme_Holo_Dialog_MinWidth,
+                mDateSetListener,
+                year, month, day);
+
+        datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        datePickerDialog.show();
+    }
+
+    // Show transferred to the analysis lab dialog
+    private void showTransferredToTheAnalysisLabDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_transferred_to_the_analysis_lab, null);
+
+        final Spinner typeOfAnalysisSpinner = mView.findViewById(R.id.spinner_type_of_analysis);
+
+        builder.setView(mView);
+        builder.setTitle("Transferred to the analysis lab");
+        builder.setPositiveButton("Transfer", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                patientRegistration();
+            }
+        });
+        builder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.show();
+    }
+
+    // Transferred to the analysis lab
+    private void transferredToTheAnalysisLab() {
 
     }
 
