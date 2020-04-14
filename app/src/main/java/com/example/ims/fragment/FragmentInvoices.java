c5d57b1aaeef3b2d5e4ae7be0c3743fa507c365b
mah99239 <30526294+mah99239@users.noreply.github.com>
2020-04-14 08:54:55 +0200

    edit name view layout invoics

--- app/src/main/java/com/example/ims/fragment/FragmentInvoices.java
+++ app/src/main/java/com/example/ims/fragment/FragmentInvoices.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -22,12 +24,25 @@ public class FragmentInvoices extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    Button billSaveButton;
+    Button svcAddButton;
+    Button totalAddButton;
+    Button questionSendButton;
+
+
     private InvoicesCursorAdapter mInvoicesSvcCursorAdapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+
+public void init (ViewGroup viewGroup){
+    billSaveButton =viewGroup.findViewById(R.id.button_billtosave);
+    svcAddButton=viewGroup.findViewById(R.id.button_svcadd);
+    totalAddButton=viewGroup.findViewById(R.id.button_totelsave);
+    questionSendButton=viewGroup.findViewById(R.id.button_questionssend);
+}
     public FragmentInvoices() {
         // Required empty public constructor
     }
@@ -59,7 +74,22 @@ public class FragmentInvoices extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        init(container);
+
+
+
+        billSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
 
         return inflater.inflate(R.layout.fragment_invoices, container, false);
+
+
+
     }
 }
