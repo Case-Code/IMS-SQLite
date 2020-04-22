4642f75c6c15a1befd3204b04ca0edba96c8a7a4
mah99239 <30526294+mah99239@users.noreply.github.com>
2020-04-22 05:50:04 +0200

    Improved activity personnel #1

--- app/src/main/java/com/example/ims/PersonnelActivity.java
+++ app/src/main/java/com/example/ims/PersonnelActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -20,6 +26,32 @@ public class PersonnelActivity extends AppCompatActivity implements NavigationVi
     private NavigationView mNavigationView;
     private ImageButton mActionMenuImageButton;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    //Employee data management
+    private AutoCompleteTextView edmEmployeeSearchAutoCompleteTextView;
+    private ListView edmPatientListView;
+
+    //Attendance and Departure
+    private AutoCompleteTextView aadDateSearchAutoCompleteTextView;
+    private ListView aadDateListView;
+
+    //Employee
+    private EditText employeeFirstNameEditText;
+    private EditText employeeLastNameEditText;
+    private EditText employeeEmailEditText;
+    private EditText employeePhoneEditText;
+    private TextView employeeHireDateTextView;
+    private EditText employeeSalaryEditText;
+    private Spinner employeeDepartmentSpinner;
+    private EditText employeeJobTitleEditText;
+    private EditText employeeMinSalaryEditText;
+    private EditText employeeMaxSalaryEditText;
+    private Spinner employeeRegionNameSpinner;
+    private Spinner employeeCountryNameSpinner;
+    private Spinner employeeCitySpinner;
+    private EditText employeeStreetAddressEditText;
+    private EditText employeePostalCodeEditText;
+    private Button employeeAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,5 +130,30 @@ public class PersonnelActivity extends AppCompatActivity implements NavigationVi
         mDrawerLayout = findViewById(R.id.activity_Personnel);
         mNavigationView = findViewById(R.id.navigation_view);
         mActionMenuImageButton = findViewById(R.id.image_button_action_menu);
+
+         edmEmployeeSearchAutoCompleteTextView=findViewById(R.id.text_edm_employee_search);
+         edmPatientListView=findViewById(R.id.list_edm_patient);
+
+        //Attendance and Departure
+         aadDateSearchAutoCompleteTextView=findViewById(R.id.text_aad_date_search);
+         aadDateListView=findViewById(R.id.list_aad_date);
+
+        //Employee
+         employeeFirstNameEditText=findViewById(R.id.edit_employee_firstname);
+         employeeLastNameEditText=findViewById(R.id.edit_employee_lastname);
+         employeeEmailEditText=findViewById(R.id.edit_employee_email);
+         employeePhoneEditText=findViewById(R.id.edit_employee_phone);
+         employeeHireDateTextView=findViewById(R.id.text_employee_hire_date);
+         employeeSalaryEditText=findViewById(R.id.edit_employee_salary);
+         employeeDepartmentSpinner=findViewById(R.id.spinner_employee_department);
+         employeeJobTitleEditText=findViewById(R.id.edit_employee_job_title);
+         employeeMinSalaryEditText=findViewById(R.id.edit_employee_min_salary);
+         employeeMaxSalaryEditText=findViewById(R.id.edit_employee_max_salary);
+         employeeRegionNameSpinner=findViewById(R.id.spinner_employee_region_name);
+         employeeCountryNameSpinner=findViewById(R.id.spinner_employee_country_name);
+         employeeCitySpinner=findViewById(R.id.spinner_employee_city);
+         employeeStreetAddressEditText=findViewById(R.id.edit_employee_street_address);
+         employeePostalCodeEditText=findViewById(R.id.edit_employee_postal_code);
+         employeeAddButton=findViewById(R.id.button_employee_add);
     }
 }
